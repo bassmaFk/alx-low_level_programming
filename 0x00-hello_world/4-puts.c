c331887d624aac538635360149df1344aaf6cab7
bassmaFK <bassmafokahi6@gmail.com>
2023-04-20 14:14:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
- * main - print a line of code using puts
+ * main -Print a line of code using puts
  *
- * return:0
+ * return:|0
  */
-int main(void)
-	puts("\|""Programming is like building a multilingual puzzle");
+int main(void) 
+{	
+	puts("\""Programming is like building a multilingual puzzle");
 	return (0);
+}	
