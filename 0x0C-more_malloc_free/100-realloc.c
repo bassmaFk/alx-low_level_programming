247cb038c306fde234a46afba2940e2936d52780
bassmaFK <bassmafokahi6@gmail.com>
2023-05-18 06:00:06 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,8 +5,7 @@
 * _realloc - reallocates a memory block.
 * @ptr: pointer to the memory previously allocated.
 * @old_size: size, in bytes, of the allocated space of ptr.
-* @new_size: new size, in bytes, of the new memory block. 
-*
+* @new_size: new size, in bytes, of the new memory block.
 * Return: ptr.
 * if new_size == old_size, returns ptr without changes.
 * if malloc fails, returns NULL.
